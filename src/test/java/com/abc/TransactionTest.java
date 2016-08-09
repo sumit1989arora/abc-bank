1b86af6b2a492671e5301221f25e56df5a3d9e8b
sumit1989arora <sumit.1989.arora@gmail.com>
2016-08-08 21:33:36 -0400

    Update TransactionTest.java

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -4,10 +4,15 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Date;
+
 public class TransactionTest {
     @Test
     public void transaction() {
-        Transaction t = new Transaction(5);
+    	Date dt = new Date();
+    	DateProvider ft = new DateProvider();
+    	dt=ft.now();
+        Transaction t = new Transaction(5,dt);
         assertTrue(t instanceof Transaction);
     }
 }
