ee5718d2472a94fa47d207036fef566730175fb8
sumit1989arora <sumit.1989.arora@gmail.com>
2016-08-08 21:33:00 -0400

    Update BankTest.java

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -4,8 +4,13 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class BankTest {
     private static final double DOUBLE_DELTA = 1e-15;
+    Date dt = new Date();
+	DateProvider ft = new DateProvider();
 
     @Test
     public void customerSummary() {
@@ -23,8 +28,9 @@ public class BankTest {
         Account checkingAccount = new Account(Account.CHECKING);
         Customer bill = new Customer("Bill").openAccount(checkingAccount);
         bank.addCustomer(bill);
-
-        checkingAccount.deposit(100.0);
+       
+    	dt=ft.now();
+        checkingAccount.deposit(100.0,dt);
 
         assertEquals(0.1, bank.totalInterestPaid(), DOUBLE_DELTA);
     }
@@ -34,8 +40,8 @@ public class BankTest {
         Bank bank = new Bank();
         Account checkingAccount = new Account(Account.SAVINGS);
         bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
-
-        checkingAccount.deposit(1500.0);
+        dt=ft.now();
+        checkingAccount.deposit(1500.0,dt);
 
         assertEquals(2.0, bank.totalInterestPaid(), DOUBLE_DELTA);
     }
@@ -45,10 +51,43 @@ public class BankTest {
         Bank bank = new Bank();
         Account checkingAccount = new Account(Account.MAXI_SAVINGS);
         bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
+        dt=ft.now();
+        checkingAccount.deposit(3000.0,dt);
 
-        checkingAccount.deposit(3000.0);
+        assertEquals(3.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+    }
+    
+    
+    @Test
+    public void maxi_savings_accountOldTransaction() {
+        Bank bank = new Bank();
+        Account checkingAccount = new Account(Account.MAXI_SAVINGS);
+        bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
+        dt=ft.now();
+        
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.HOUR, -550);
+        
+        checkingAccount.deposit(3000.0,cal.getTime());
+        
 
-        assertEquals(170.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+        assertEquals(150.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+    }
+    
+    @Test
+    public void maxi_savings_accountOldCurrentTransaction() {
+        Bank bank = new Bank();
+        Account checkingAccount = new Account(Account.MAXI_SAVINGS);
+        bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
+        dt=ft.now();
+        
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.HOUR, -550);
+        
+        checkingAccount.deposit(3000.0,cal.getTime());
+        checkingAccount.deposit(3000.0,dt);
+
+        assertEquals(6.0, bank.totalInterestPaid(), DOUBLE_DELTA);
     }
 
 }
