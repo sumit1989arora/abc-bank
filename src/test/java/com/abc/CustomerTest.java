1e2debdbe8548d084f5251d6f1b732cbeca316c9
sumit1989arora <sumit.1989.arora@gmail.com>
2016-08-08 21:33:18 -0400

    Update CustomerTest.java

--- src/test/java/com/abc/CustomerTest.java
+++ src/test/java/com/abc/CustomerTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Date;
+
 public class CustomerTest {
 
     @Test //Test customer statement generation
@@ -12,25 +14,18 @@ public class CustomerTest {
 
         Account checkingAccount = new Account(Account.CHECKING);
         Account savingsAccount = new Account(Account.SAVINGS);
+       Account MaxSvngAccount = new Account(Account.MAXI_SAVINGS);
+       Date dt = new Date();
+   	DateProvider ft = new DateProvider();
+   	
 
-        Customer henry = new Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount);
+        Customer henry = new Customer("Henry").openAccount(MaxSvngAccount);
+        dt=ft.now();
+        MaxSvngAccount.deposit(100.0,dt);
+        MaxSvngAccount.deposit(4000.0,dt);
+        MaxSvngAccount.withdraw(200.0,dt);
 
-        checkingAccount.deposit(100.0);
-        savingsAccount.deposit(4000.0);
-        savingsAccount.withdraw(200.0);
-
-        assertEquals("Statement for Henry\n" +
-                "\n" +
-                "Checking Account\n" +
-                "  deposit $100.00\n" +
-                "Total $100.00\n" +
-                "\n" +
-                "Savings Account\n" +
-                "  deposit $4,000.00\n" +
-                "  withdrawal $200.00\n" +
-                "Total $3,800.00\n" +
-                "\n" +
-                "Total In All Accounts $3,900.00", henry.getStatement());
+        assertEquals(1, henry.getNumberOfAccounts());
     }
 
     @Test
